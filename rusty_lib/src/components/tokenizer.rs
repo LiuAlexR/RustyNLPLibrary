c60ef8fb3d547748c5f133ef80453f40933243d9
Alex Liu <alexliu3000@gmail.com>
2026-08-08 09:39:56 -0600

    Replaced initial vocabulary with all ASCII characters

--- rusty_lib/src/components/tokenizer.rs
+++ rusty_lib/src/components/tokenizer.rs
@@ -4,17 +4,7 @@ use std::{collections::HashMap, vec::Vec};
 /// @param corpus - text to tokenize
 /// @param num_tokens - maximum number of tokens in vocabulary plus default ASCII
 pub fn bpe_tokenize(corpus: &str, num_tokens: u64) -> Vec<String> {
-    let mut vocabulary: Vec<String> = vec![
-        " ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1",
-        "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C",
-        "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
-        "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g",
-        "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y",
-        "z", "{", "|", "}", "~",
-    ]
-    .into_iter()
-    .map(String::from)
-    .collect();
+    let mut vocabulary: Vec<String> = (0..128).map(|b: u8| (b as char).to_string()).collect();
 
     let arr: Vec<char> = corpus.chars().collect();
     for _ in 0..num_tokens {
