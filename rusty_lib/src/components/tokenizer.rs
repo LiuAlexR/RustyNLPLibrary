5d3422a4d86d72b504be42957b4fec3fc123ab03
TheSilentIce <srikaryadlapati@gmail.com>
2026-08-07 22:03:36 -0700

    Byte-Pair Encoding Implemented
    Closes #1

--- rusty_lib/src/components/tokenizer.rs
+++ rusty_lib/src/components/tokenizer.rs
@@ -1,64 +1,82 @@
 use std::{collections::HashMap, vec::Vec};
 
-// corpus is going to be just the direct txt file into a string
-// num tokens - number of tokens we want to add atop the default ascii chars
-//
-// split corpus into chars
-//
-// a s d f => as d f => asd f
-pub fn tokenize(corpus: &str, num_tokens: u64) -> Vec<&str> {
-    let mut vocabulary: Vec<&str> = vec![
+/// Byte-Pair Encoding scheme
+/// @param corpus - text to tokenize
+/// @param num_tokens - maximum number of tokens in vocabulary plus default ASCII
+pub fn bpe_tokenize(corpus: &str, num_tokens: u64) -> Vec<String> {
+    let mut vocabulary: Vec<String> = vec![
         " ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1",
         "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C",
         "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U",
         "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g",
         "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y",
         "z", "{", "|", "}", "~",
-    ];
+    ]
+    .into_iter()
+    .map(String::from)
+    .collect();
 
-    let arr: Vec<&str> = corpus.split("").collect();
+    let arr: Vec<char> = corpus.chars().collect();
+    for _ in 0..num_tokens {
+        let token = combine(&arr, &vocabulary);
 
-    for i in 0..num_tokens {
-        // iterate through arr
-        // for combining tokens, use a while loop alongside find_token method to see if token exists
-        // then, once you have an existing token, add the next char to it, then update map
-        //
-        // once you do that for arr, return most frequent token, on ties, we'll just add all of them
-        // let max = m.iter().max();
-        // make sure that you do not combine words with a space between them
-        //
-        // add token to vocab and clear the map
-        // m.clear()
+        if let Some(token) = token {
+            vocabulary.push(token)
+        }
     }
 
     vocabulary
 }
 
-fn combine<'a>(arr: &'a [&str]) -> &'a str {
-    let mut m: HashMap<&str, u64> = Default::default();
+fn combine(arr: &[char], vocabulary: &[String]) -> Option<String> {
+    let mut map: HashMap<String, u64> = Default::default();
 
     let len: u64 = arr.len() as u64;
     let mut i = 0;
 
+    let mut token: String = String::default();
+
     while i < len {
-        i += 1
+        if arr[i as usize] == ' ' {
+            i += 1;
+            token.clear();
+            continue;
+        }
+
+        token.push(arr[i as usize]);
+
+        if has_token(&token, vocabulary) {
+            i += 1;
+            continue;
+        }
+
+        *map.entry(token.clone()).or_insert(0) += 1;
+        token.clear();
     }
 
-    let (x, _) = match m.iter().max_by_key(|&(_, y)| y) {
+    if map.is_empty() {
+        return None;
+    }
+
+    // for ties, returns the last one
+    // not deterministic, as pointers are stored anywhere
+    let (x, _) = match map.iter().max_by_key(|&(_, y)| y) {
         None => panic!("Error finding max of token map"),
         Some((x, y)) => (x, y),
     };
 
-    x
+    Some(x.clone())
 }
 
 // may want to use a better algorithm
-fn find_token(token: &str, vocabulary: &[&str]) -> bool {
+fn has_token(token: &str, vocabulary: &[String]) -> bool {
     for n in vocabulary {
-        if *n == token {
+        if n == token {
             return true;
         }
     }
 
     false
 }
+
+// TODO(LiuAlexR) - implement function to tokenize future inputs
