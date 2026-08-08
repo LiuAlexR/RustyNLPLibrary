532b6b3c00d491c9b21cfe7cc55cf9c9e2ec9ef7
LiuAlexR <alexliu3000@gmail.com>
2026-08-07 22:18:01 -0600

    Finish exports and importers

--- rusty_lib/src/main.rs
+++ rusty_lib/src/main.rs
@@ -1,6 +1,7 @@
 // use rusty_lib::components::*;
 use rusty_lib::util;
 fn main() { 
-    let x = util::retrieve_source("orwell_1984.txt");
+    let x = util::retrieve_source("liu_hello_world.txt");
     println!("{x}");
+    let _ = util::write_to_storage("test.txt", &"boo\nbooo".to_string());
 }
--- rusty_lib/src/util.rs
+++ rusty_lib/src/util.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, path::Path};
+use std::{env, fs, io, path::Path};
 
 /// Reads the file name matching file_path within the 'texts' directory, found in the root of the Github repo. Used for development purposes.
 /// Arguments: the file name, e.g., "orwell_1984.txt"
@@ -38,4 +38,36 @@ pub fn retrieve_source(file_path: &str) -> String {
     };
     // returns the String
     file_contents
+}
+
+/// Writes a String to a file within the 'storage' folder in the root. Will override if the file already exists, and will create the file if it does not.
+pub fn write_to_storage(file_path: &str, contents: &String) -> io::Result<()> {
+    // 'path' becomes the root of the Rust file; e.g., path = path/to/project/rusty_lib
+    let path = match env::current_dir() {
+        Ok(x) => x,
+        Err(_) => {
+            panic!("Error reading current path. Something went wrong.");
+        },
+    };
+    // project_root is the Git source, it is also where we navigate to the texts
+    let path_parent: &Path = match path.parent() {
+        Some(found_path) => found_path,
+        None => {
+            panic!("Error navigating to project root.");
+        },
+    };
+    // nagivates and selects the file
+    let mut text_path = path_parent.to_path_buf();
+    text_path.push("storage");
+    text_path.push(file_path);
+    // Writes the file, and returns the Result of the operation
+    fs::write(text_path, contents)
+}
+/// Joins a vector to a String
+pub fn vec_to_string(delimiter: &str, the_vec: &Vec<String>) -> String {
+    the_vec.join(delimiter)
+}
+
+pub fn string_to_vec(delimiter: &str, the_string: &String) -> Vec<String> {
+    the_string.split(delimiter).map(String::from).collect()
 }
