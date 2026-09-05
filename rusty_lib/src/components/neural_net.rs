8d33df26458e8ad8b10746e1dffc5cc7a27c9008
TheSilentIce <srikaryadlapati@gmail.com>
2026-09-04 20:23:09 -0700

    finished implementing core neural net functions

--- rusty_lib/src/components/neural_net.rs
+++ rusty_lib/src/components/neural_net.rs
@@ -1,7 +1,10 @@
 // This neural net implementation is only going to handle 2 layers
 // Since the Feedforward layer for a Transformer block is only 2 layers
 
-use crate::math::{create_random_matrix, Backend};
+use crate::{
+    components::logistic_regression::add_bias,
+    math::{create_random_matrix, create_random_vector, Backend},
+};
 use burn::{
     tensor::{
         activation::{relu, softmax},
@@ -12,8 +15,8 @@ use burn::{
 use std::collections::HashMap;
 
 const LEARNING_RATE: f64 = 3.4;
-const NUM_LAYERS: i64 = 3;
-const NUM_HIDDEN_NODES: i64 = 10;
+const NUM_HIDDEN_NODES: i64 = 3;
+const EPOCHS: i64 = 2;
 
 type Activator = fn(Tensor<Backend, 2>) -> Tensor<Backend, 2>;
 
@@ -25,7 +28,28 @@ pub fn use_relu(weights: Tensor<Backend, 2>) -> Tensor<Backend, 2> {
     relu(weights)
 }
 
-pub fn train(X: Tensor<Backend, 2>, y: Tensor<Backend, 2>) {}
+pub fn train(
+    X: Tensor<Backend, 2>,
+    y: Tensor<Backend, 1>,
+    weights: Option<Vec<Tensor<Backend, 2>>>,
+    vocab_size: i64,
+    act: Activator,
+) -> (Tensor<Backend, 2>, Tensor<Backend, 2>) {
+    let X = add_bias(X);
+
+    let (mut W, mut U) = match weights {
+        Some(w) => (w[0].clone(), w[1].clone()),
+        None => (
+            create_random_matrix(X.dims()[1] as i64, NUM_HIDDEN_NODES).require_grad(),
+            create_random_matrix(NUM_HIDDEN_NODES, vocab_size).require_grad(),
+        ),
+    };
+
+    for _ in 0..EPOCHS {
+        (W, U) = one_pass(X.clone(), y.clone(), W, U, act);
+    }
+    (W, U)
+}
 
 pub fn one_pass(
     X: Tensor<Backend, 2>,
@@ -72,7 +96,7 @@ pub fn backward_pass(
     (W_new, U_new)
 }
 
-fn concatenate_embeddings(
+pub fn concatenate_embeddings(
     input: &[String],
     token_map: &HashMap<String, i64>,
     embedding_matrix: Tensor<Backend, 2>,
