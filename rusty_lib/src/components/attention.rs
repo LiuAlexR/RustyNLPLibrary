93ac2c0c479e70ed30b91aa3ef8f25da0fa696d4
TheSilentIce <srikaryadlapati@gmail.com>
2026-09-08 16:57:36 -0700

    implemented sinusoidial positional embeddings again

--- rusty_lib/src/components/attention.rs
+++ rusty_lib/src/components/attention.rs
@@ -15,3 +15,47 @@
 // FFN(x) = ReLU(xW1 + b1)W2 + b2
 // where x is the input of the activation function
 // W1 and W2 are matrices, and b1 and b2 are bias vectors
+//
+
+use std::collections::HashMap;
+
+use crate::math::Backend;
+use burn::{
+    tensor::{Float, TensorData},
+    Tensor,
+};
+
+const DK: i64 = 64;
+const DV: i64 = 64;
+const D: i64 = 512;
+
+// Implement method to create final input matrix of [Nxd]
+// Then method to calculate attention
+//  make sure to save weights in a vector or somewhere,
+//  remember, Q,K,V weights for each head, in each block
+//  If there are 5 heads and 4 blocks, then there are a total of
+//  3 x 5 x 4 = 60 weight matrices
+// Implement LayerNorm
+
+/// Generates sinusoidal position embeddings for `length` tokens of dimensionality `d`
+///
+/// Returns a 2D Tensor where each row vector `i` is the positional embedding for the `ith` token
+pub fn generate_positional_embeddings(length: usize, d: usize) -> Tensor<Backend, 2> {
+    let mut res: Vec<Vec<f64>> = vec![vec![]; length];
+
+    for pos in 0..length {
+        let pos = pos as f64;
+        let mut v: Vec<f64> = vec![0.; d];
+
+        for i in 0..d / 2 {
+            let denominator = 10000_f64.powf(2. * i as f64 / d as f64);
+            v[2 * i] = (pos / denominator).sin();
+            v[2 * i + 1] = (pos / denominator).cos();
+        }
+        res[pos as usize] = v.clone();
+    }
+
+    let shape = [length, d];
+    let t = TensorData::new(res.into_iter().flatten().collect(), shape);
+    Tensor::<Backend, 2>::from_data(t, &Default::default())
+}
